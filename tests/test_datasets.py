5d4dd09900d2fad11b43e73c0f4294dc724c5723
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-10 10:43:49 +0530

    refactor

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -12,28 +12,16 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from dataclasses import asdict, dataclass
-
-import requests
 import typer
+from typer.testing import CliRunner
 
-from gflow_cli.constants import DATASETS_URL
+from gflow_cli.datasets import get_available_tasks
 
-app = typer.Typer(help="Manage your datasets with `gflow-cli datasets` command.")
-
-AVAILABLE_STORAGE = ("LOCAL", "REMOTE")
-AVAILABLE_TASKS = ("IMAGE-CLASSIFICATION",)
-AVAILABLE_DATASET_TYPES = ("FROM-FOLDER", "FROM-CSV")
+runner = CliRunner()
 
 
-@dataclass
-class DatasetRequest:
-    path: str
-    task: str
-    dataset_type: str
-    remote: bool
-
-
-@app.command(name="available-tasks")
-def get_available_tasks() -> None:
-    typer.echo(AVAILABLE_TASKS)
+def test_add_project():
+    app = typer.Typer()
+    app.command()(get_available_tasks)
+    result = runner.invoke(app)
+    assert result.exit_code == 0
